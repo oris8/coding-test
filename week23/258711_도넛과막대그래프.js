e65df326e684ad55172ecb34a341a3c139be84fd
eprnf <eprnf21@gmail.com>
2024-09-09 15:55:28 +0900

    fixTypo

--- week23/258711_도넛과막대그래프.js
+++ week23/258711_도넛과막대그래프.js
@@ -1,6 +1,6 @@
 function solution(edges) {
-  const graph = {};
-  const answer = [0, 0, 0, 0];
+  const graph = {}; // 각 정점에 들어오고 나가는 간선 수를 계산
+  const answer = [0, 0, 0, 0]; // [생성된정점,도넛,막대,8자모양]
 
   edges.forEach(([a, b]) => {
     if (!graph[a]) graph[a] = { inCount: 0, outCount: 0 };
@@ -9,22 +9,24 @@ function solution(edges) {
     graph[b].inCount++;
   });
 
+  const check = (vertex) => {
+    // 그래프의 모양을 체크하는 함수
+    const { inCount, outCount } = graph[vertex];
+
+    if (outCount >= 2 && inCount === 0) {
+      // 들어오는게 없고, 나가는게 2이상일 경우 생성된 정점임)
+      answer[0] = +vertex;
+      answer[1] = outCount; // 도넛 카운트에 임시 할당
+    }
+    if (outCount === 0) {
+      answer[2] += 1; // 막대
+    }
+    if (inCount >= 2 && outCount >= 2) {
+      answer[3] += 1; // 차수가 4인 경우 8자 모양의 중심(생성된 정점에 연결되어있는경우를 고려해 2이상으로 조건문 설정)
+    }
+  };
+
   Object.keys(graph).forEach((vertex) => {
-    const check = (vertex) => {
-      const { inCount, outCount } = graph[vertex];
-
-      if (sendCount >= 2 && receiveCount === 0) {
-        answer[0] = +vertex;
-        answer[1] = outCount;
-      }
-      if (outCount === 0) {
-        answer[2] += 1; // 막대
-      }
-      if (inCount >= 2 && outCount >= 2) {
-        answer[3] += 1; // 차수가 4인 경우 8자 모양의 중심(생성된 정점에 연결되어있는경우를 고려해 2이상으로 조건문 설정)
-      }
-    };
-
     check(vertex);
   });
 
